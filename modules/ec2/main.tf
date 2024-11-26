820983bce30cb3afc55e5ef23839d76062f07449
fabricioqueiroz <106182132+fabricioqueiroz@users.noreply.github.com>
2024-11-26 15:15:00 -0300

    Update main.tf
    Add security groups

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -30,6 +30,20 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["177.125.187.118/32"] 
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
